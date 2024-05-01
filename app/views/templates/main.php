afc5cacabd27701574b8313750fe72919cddac29
johlbaum <julien.ohlbaum@gmail.com>
2024-05-01 16:15:06 +0200

    Added header to main template

--- app/views/templates/main.php
+++ app/views/templates/main.php
@@ -4,14 +4,34 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
     <title><?= $title ?></title>
     <link rel="stylesheet" href="./css/style.css">
 </head>
 
 <body>
-    <p>Nav</p>
-    <?= $content ?>
-    <p>Footer</p>
+    <div class="main-container">
+        <header>
+            <div class="logo">
+                <img src="./img/logo.svg" alt="Le logo de Tom Troc" />
+            </div>
+            <nav>
+                <ul class="left-menu">
+                    <li><a href="#">Accueil</a></li>
+                    <li><a href="#">Nos livres à l’échange</a></li>
+                </ul>
+                <ul class="right-menu">
+                    <li><a href="#">Messagerie</a></li>
+                    <li><a href="#">Mon compte</a></li>
+                    <li><a href="#">Connexion</a></li>
+                </ul>
+            </nav>
+        </header>
+        <?= $content ?>
+        <p>Footer</p>
+    </div>
 </body>
 
 </html>
